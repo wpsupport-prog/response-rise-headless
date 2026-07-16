ab089e6de110a56723b2265c14d97f3470b6b3f8
unknown <wpsupport@justwpsupport.com>
2026-07-16 19:56:50 +0800

    build: bypass ESLint warnings for production and fix duplicate Script import

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
 import Script from 'next/script';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { getPageAssetsAndHtml, getAllPagesWithSlugs } from '../lib/api';
